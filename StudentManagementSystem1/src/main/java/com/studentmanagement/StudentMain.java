fab9d08054bb8b332ccef46c7d4608cf7067d47d
vanitha1991 <vanitha.g10@gmail.com>
2023-07-18 13:48:24 +0530

    Hibernate project

---
+++ StudentManagementSystem1/src/main/java/com/studentmanagement/StudentMain.java
@@ -0,0 +1,22 @@
+package com.studentmanagement;
+
+import java.util.List;
+import java.util.Scanner;
+
+import com.studentmanagement.entity.Student;
+import com.studentmanagement.service.StudentService;
+import com.studentmanagement.serviceimpl.StudentServiceImpl;
+import com.studentmanagement.utility.Helper;
+
+public class StudentMain 
+{
+	
+    public static void main( String[] args )
+    {
+        //creating an object of student service
+    	
+    	Helper.menu();
+    	
+    	
+    }
+}
